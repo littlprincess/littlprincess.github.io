4b4795934ae8985d0362749ad39c86497982544c
littlprincess <smolemiirose@gmail.com>
2024-10-29 23:07:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var discordInvite = "https://discord.com/invite/BRudbsetbu";
+
+function hideAgeWarning() {
+    document.getElementsByTagName("body")[0].style.overflow = "auto";
+    document.getElementById("age-warning").style.display = "none";
+}
+
+function discordRedirect() {
+    window.open(discordInvite, '_blank');
+}
+
+function onLoad() {
+    window.scrollTo(0,0);
+    document.getElementsByTagName("body")[0].style.overflow = "hidden";
+}
